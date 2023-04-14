2df4116ed8dfe22f3d95c9af0bc09be53571e8ca
N1COLASSS <nicolascg18@gmail.com>
2023-04-14 18:30:13 -0400

    Login
    Cree view y controller

---
+++ Peluqueria-Calibrum/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PeluqueriaCalibrum.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
