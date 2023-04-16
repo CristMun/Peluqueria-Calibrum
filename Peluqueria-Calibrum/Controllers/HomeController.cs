3dcf93773cc56115b1b164635096e13eca83b6b4
EliR21 <103208430+EliR21@users.noreply.github.com>
2023-04-16 16:37:56 -0400

    Avance

--- Peluqueria-Calibrum/Controllers/HomeController.cs
+++ Peluqueria-Calibrum/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Peluqueria_Calibrum.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
