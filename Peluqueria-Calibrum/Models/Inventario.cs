dd7b2f92255ab23b6013f469f01beb977b9fead0
N1COLASSS <nicolascg18@gmail.com>
2023-04-18 23:02:10 -0400

    Nuevo Articulo
    Modal nuevo articulo creado

--- Peluqueria-Calibrum/Models/Inventario.cs
+++ Peluqueria-Calibrum/Models/Inventario.cs
@@ -1,12 +1,22 @@
-﻿namespace Peluqueria_Calibrum.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Peluqueria_Calibrum.Models
 {
     public class Inventario
     {
+        [Key]
         public int Id_inventario { get; set; }
 
-        //public string Imagen { get; set; }
-        public string Nombre_art { get; set; }
-        public string Descripcion { get; set; }
+        public string? Imagen { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string? Nombre_art { get; set; }
+
+        [Required]
+        public string? Descripcion { get; set; }
+
+        [Required]
         public int Cantidad { get; set; }
 
     }
