a51cec481f818a3be6e6dcf2349a4a8578c87f3c
EliR21 <eliasrojasmora21@gmail.com>
2023-04-20 15:00:21 -0400

    Vista Clientes inicio

---
+++ Peluqueria-Calibrum/Controllers/Clientes.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Peluqueria_Calibrum.Controllers
+{
+    public class ClientesController : Controller
+    {
+        public IActionResult Clientes()
+        {
+            return View();
+        }
+    }
+}
