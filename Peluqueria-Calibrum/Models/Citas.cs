e638914362058e3910873de02f6e5c5c7fd9dd47
N1COLASSS <nicolascg18@gmail.com>
2023-04-20 17:13:41 -0400

    Vista Citas
    Vista y crear nueva cita

--- Peluqueria-Calibrum/Models/Citas.cs
+++ Peluqueria-Calibrum/Models/Citas.cs
@@ -3,12 +3,12 @@
     public class Citas
     {
         public int Id_cita { get; set; }
-        public string Hora_cita { get; set; }
-        public string Nombre_servicio { get; set; }
-        public string Nombre { get; set; }
-        public string Nombre_cliente { get; set; }
-        public string Telefono { get; set; }
-        public int Precio_total { get; set; }
+        public string? Hora_cita { get; set; }
+        public string? Nombre_servicio { get; set; }
+        public string? Nombre_pelu { get; set; }
+        public string? Nombre_cliente { get; set; }
+        public string? Telefono { get; set; }
+        public int? Precio_total { get; set; }
 
     }
 }
