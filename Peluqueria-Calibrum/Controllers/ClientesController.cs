d672c039396de41f016b031db1cc6aa15bebb075
EliR21 <eliasrojasmora21@gmail.com>
2023-04-20 20:54:11 -0400

    Update ClientesController.cs

--- Peluqueria-Calibrum/Controllers/ClientesController.cs
+++ Peluqueria-Calibrum/Controllers/ClientesController.cs
@@ -4,7 +4,15 @@ namespace Peluqueria_Calibrum.Controllers
 {
     public class ClientesController : Controller
     {
-        public IActionResult Clientes()
+        public IActionResult Agendar()
+        {
+            return View();
+        }
+        public IActionResult Peluqueros()
+        {
+            return View();
+        }
+        public IActionResult Principal()
         {
             return View();
         }
