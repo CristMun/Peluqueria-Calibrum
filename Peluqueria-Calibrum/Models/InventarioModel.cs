d47192ae7a99ea4d6cdca3eb9102e7e0cc392534
N1COLASSS <nicolascg18@gmail.com>
2023-05-07 21:21:35 -0400

    Modal

--- Peluqueria-Calibrum/Models/InventarioModel.cs
+++ Peluqueria-Calibrum/Models/InventarioModel.cs
@@ -6,8 +6,8 @@ namespace Peluqueria_Calibrum.Models
     {
         public int Id { get; set; }
         public string Imagen { get; set; }
-        public string Nombre { get; set; }
-        public string Descripcion { get; set; }
+        public string? Nombre { get; set; }
+        public string? Descripcion { get; set; }
         public int Cantidad { get; set; }
 
     }
