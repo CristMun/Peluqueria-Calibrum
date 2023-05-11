ef8daf07094543bdb3c74d18162b17e5113ab7ff
N1COLASSS <nicolascg18@gmail.com>
2023-05-10 21:03:15 -0400

    Insert Delete Inventario

--- Peluqueria-Calibrum/Controllers/InventarioController.cs
+++ Peluqueria-Calibrum/Controllers/InventarioController.cs
@@ -2,13 +2,12 @@
 using Microsoft.AspNetCore.Mvc;
 using MySql.Data.MySqlClient;
 using Peluqueria_Calibrum.Models;
+using System.Net.NetworkInformation;
 
 namespace Peluqueria_Calibrum.Controllers
 {
     public class InventarioController : MyController
     {
-        private string _connection = @"Server=sql777.main-hosting.eu;Database=u364986239_calibrum;Uid=u364986239_admin_calibrum;Password=2d839@sT";
-
         [Route("Inventario")]
         public IActionResult Inventario()
         {
@@ -20,12 +19,51 @@ namespace Peluqueria_Calibrum.Controllers
         public IActionResult GetInventario()
         {
             IEnumerable<InventarioModel> lst = null;
-            using (var db = new MySqlConnection(_connection))
+            using (var db = new MySqlConnection(MyController.csCal))
             {
                 var sql = "SELECT * FROM Inventario";
                 lst = db.Query<InventarioModel>(sql);
             }
             return View(lst);
+        }
+
+        /*Metodo para ingresar datos en la base de datos*/
+        [HttpPost]
+        
+        public IActionResult Insert(Models.InventarioModel model)
+        {
+            int result = 0;
+            using (var db = new MySqlConnection(MyController.csCal))
+            {
+                var sql = "INSERT INTO Inventario(Nombre, Descripcion, Cantidad) " +
+                    " VALUES (@Nombre, @Descripcion, @Cantidad)";
+                result = db.Execute(sql, model);
+            }
+            return RedirectToAction("Inventario");
+        }
+
+        [HttpPut]
+        public IActionResult Edit(Models.InventarioModel model)
+        {
+            int result = 0;
+            using (var db = new MySqlConnection(MyController.csCal))
+            {
+                var sql = "UPDATE Inventario SET Nombre=@Nombre, Descripcion=@Descripcion, Cantidad=@Cantidad";
+                result = db.Execute(sql, model);
+            }
+            return RedirectToAction("Inventario");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            int result = 0;
+            using (var db = new MySqlConnection(MyController.csCal))
+            {
+                var sql = "DELETE FROM Inventario WHERE Id=@id";
+                result = db.Execute(sql, new { id });
+            }
+            return RedirectToAction("Inventario");
         }
     }
 }
